efcba52c67923b037de8df876e584f92d1caf6f7
suki186 <ksh.1205263@gmail.com>
2026-04-03 05:01:31 +0900

    [feat] - Spinner 컴포넌트 구현 및 방명록에 적용

---
+++ app/src/components/Spinner.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { Box } from '@vapor-ui/core';
+import { ClipLoader } from 'react-spinners';
+
+interface SpinnerProps {
+  size?: number;
+  color?: string;
+  loading?: boolean;
+}
+
+export default function Spinner({
+  size = 50,
+  color = '#6DBFFF',
+  loading = true,
+}: SpinnerProps) {
+  if (!loading) return null;
+
+  return (
+    <Box
+      $css={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 100000,
+        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+      }}
+    >
+      <Box
+        $css={{
+          width: '100%',
+          maxWidth: '390px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ClipLoader
+          color={color}
+          loading={loading}
+          size={size}
+          speedMultiplier={1}
+        />
+      </Box>
+    </Box>
+  );
+}
